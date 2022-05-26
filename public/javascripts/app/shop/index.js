b4bf08a582bf37231756415a42f9bdfa40552f6a
supakrit <supakrit.fl@gmail.com>
2022-05-26 22:26:45 +0700

    add jquery shop index.js

--- public/javascripts/app/shop/index.js
+++ public/javascripts/app/shop/index.js
@@ -1,10 +1,14 @@
-$(document).ready(function () {
+window.$(document).ready(function () {
   let counter = 0;
 
-  $("#counter").text(counter);
+  setCounter();
 
   $("#counter-btn").click(function (e) {
     counter += 1;
-    $("#counter").text(counter);
+    setCounter();
   });
+
+  function setCounter() {
+    $("#counter").text(counter);
+  }
 });
